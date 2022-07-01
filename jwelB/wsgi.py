fbfad58afc1b398c829d4174d69d8732c2514e15
praveenstreams <praveenkumarstream@gmail.com>
2022-07-01 15:30:37 +0530

    settings

--- jwelB/wsgi.py
+++ jwelB/wsgi.py
@@ -8,8 +8,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
-import settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jwelB.settings')
 
 application = get_wsgi_application()
