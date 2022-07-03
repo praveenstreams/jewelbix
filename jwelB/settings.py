9eb9504db3873b238e12022821fd5210a8a0abd1
praveenstreams <praveenkumarstream@gmail.com>
2022-07-03 21:01:32 +0530

    ll

--- jwelB/settings.py
+++ jwelB/settings.py
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'jwelB.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 AUTH_USER_MODEL ='user_app.UserModel'
 # Password validation
