58eecf90b491ef092b14ceac8445cdab9c076ca4
praveenstreams <praveenkumarstream@gmail.com>
2022-07-03 21:15:12 +0530

    ll

--- jwelB/settings.py
+++ jwelB/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'jwelB.wsgi.application'
 
 
 # Database
-https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # DATABASES = {
 #     'default': {
