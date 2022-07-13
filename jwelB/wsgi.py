45f9138c506bde6a8921031c2b71a0e24d5b7030
praveencodesvera <praveen.kumarn@codesvera.com>
2022-07-13 21:57:39 +0530

    db

--- jwelB/wsgi.py
+++ jwelB/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for jwelB project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+# import os
 
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jwelB.settings')
+
+# application = get_wsgi_application()
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jwelB.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jwelB.settings')
-
 application = get_wsgi_application()
